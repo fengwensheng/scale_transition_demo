e99b717924423e89c54534286007abadc6677089
Vincent Feng <fengwenshenging@gmail.com>
2020-08-10 21:26:03 +0800

    scale_transition_demo

---
+++ README.md
@@ -0,0 +1,7 @@
+# scale_transition_demo
+
+缩放动画 Demo
+
+## Getting Started
+
+![scale_transition_demo](scale_transition_demo.gif)
---
+++ lib/main.dart
@@ -0,0 +1,93 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+        visualDensity: VisualDensity.adaptivePlatformDensity,
+      ),
+      home: MyHomePage(title: 'Flutter Demo Home Page'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  MyHomePage({Key key, this.title}) : super(key: key);
+
+  final String title;
+
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> with TickerProviderStateMixin {
+  AnimationController _animationController;
+  Animation<double> _animation;
+  bool isPlaying = false;
+
+  void _incrementCounter() {
+    debugPrint('forward');
+    if (isPlaying) {
+      _animationController.stop();
+    } else {
+      _animationController.forward();
+    }
+    isPlaying = !isPlaying;
+    setState(() {});
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    _animationController = AnimationController(
+      vsync: this,
+      duration: Duration(milliseconds: 500),
+    );
+    _animation = Tween<double>(
+      begin: 0.5,
+      end: 1.0,
+    ).animate(
+      CurvedAnimation(
+        parent: _animationController,
+        curve: Curves.easeIn,
+      ),
+    );
+
+    _animation.addStatusListener((status) {
+      if (status == AnimationStatus.completed) {
+        _animationController.reverse();
+      } else if (status == AnimationStatus.dismissed) {
+        _animationController.forward();
+      }
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(widget.title),
+      ),
+      body: Center(
+        child: ScaleTransition(
+          scale: _animation,
+          child: Image.asset(
+            'assets/images/star.png',
+          ),
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: _incrementCounter,
+        tooltip: 'Increment',
+        child: Icon(isPlaying ? Icons.pause : Icons.play_arrow),
+      ),
+    );
+  }
+}
